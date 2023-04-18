231df17c3dc3133a19665ad2fd92fc228b7a5940
curtyraissa <raissacurty@gmail.com>
2023-04-17 23:57:53 -0300

    inicio dos endpoints post/get

--- src/app.js
+++ src/app.js
@@ -19,6 +19,28 @@ mongoClient.connect()
     .catch((err) => console.log(err.message))
 
 
+    // Endpoints
+    app.post("/cadastro", (req, res) => {
+        const { xxx } = req.body
+        return res.status(422).send("Todos os campos são obrigatórios!")
+    })
+    
+    app.post("/login", (req, res) => {
+        const { xxx } = req.body
+        return res.status(422).send("Todos os campos são obrigatórios!")
+    })
+    
+    app.post("/nova-transacao/:tipo", (req, res) => {
+        const { xxx } = req.body
+        return res.status(422).send("Todos os campos são obrigatórios!")
+    })
+    
+    app.get("/home", (req, res) => {
+        db.collection("home").find().toArray()
+            .then((memes) => res.status(200).send(home))
+            .catch((err) => res.status(500).send(err.message))
+    })
+
 // Deixa o app escutando, à espera de requisições
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
