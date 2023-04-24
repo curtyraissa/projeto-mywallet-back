1e03a28ea36130d7a0623bf40a705e25f61085da
curtyraissa <raissacurty@gmail.com>
2023-04-23 23:17:52 -0300

    ajuste erro token

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
-import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 import joi from "joi";
 
 // Criação do servidor
@@ -36,100 +36,99 @@ const loginSchema = joi.object({
   senha: joi.string().min(3).required(),
 });
 
-
 // Endpoints
 app.post("/cadastro", async (req, res) => {
-  const { nome, email, senha } = req.body
+  const { nome, email, senha } = req.body;
 
-  const validation = cadastroSchema.validate(req.body, {aboutEarly: false})
-  if(validation.error) {
-    res.status(422).send(validation.error.details.map(detail => detail.message))
+  const validation = cadastroSchema.validate(req.body, { aboutEarly: false });
+  if (validation.error) {
+    res
+      .status(422)
+      .send(validation.error.details.map((detail) => detail.message));
   }
-  res.sendStatus(201)
+  res.sendStatus(201);
 
-  try{
+  try {
     //verificar se esse e-mail ja foi cadastrado
-    const usuario = await db.collection("usuarios").findOne({email})
-    if(usuario) return res.status(409).send("E-mail já cadastrado")
-  
+    const usuario = await db.collection("usuarios").findOne({ email });
+    if (usuario) return res.status(409).send("E-mail já cadastrado");
+
     //criptografar senha
-    const hash =bcrypt.hashSync(senha, 10)
+    const hash = bcrypt.hashSync(senha, 10);
 
     //criar conta e guardar senha encriptografada no bd
-    await db.collection("usuarios").insertOne({nome, email, senha: hash})
-    res.status(201).send("Conta criada com sucesso!")
-  } catch (err){
-    res.status(500).send(err.message)
+    await db.collection("usuarios").insertOne({ nome, email, senha: hash });
+    res.status(201).send("Conta criada com sucesso!");
+  } catch (err) {
+    res.status(500).send(err.message);
   }
-  
 });
 
 app.post("/", async (req, res) => {
-  const { email, senha } = req.body
+  const { email, senha } = req.body;
 
-  const validation = loginSchema.validate(req.body, {aboutEarly: false})
-  if(validation.error) {
-    res.status(422).send(validation.error.details.map(detail => detail.message))
+  const validation = loginSchema.validate(req.body, { aboutEarly: false });
+  if (validation.error) {
+    res
+      .status(422)
+      .send(validation.error.details.map((detail) => detail.message));
   }
-  res.sendStatus(201)
+  res.sendStatus(201);
 
-  try{
-  //verificar se o e-mail esta cadastrado
-  const usuario = await db.collection("usuario").findOne({email})
-  if(!usuario) return res.status(404).send("E-mail não cadastrado")
+  try {
+    //verificar se o e-mail esta cadastrado
+    const usuario = await db.collection("usuarios").findOne({ email });
+    if (!usuario) return res.status(404).send("E-mail não cadastrado");
 
-  //verificar se a senha digitada corresponde com a criptografada
-  const senhaCorreta = bcrypt.compareSync(senha, usuario.senha)
-  if(!senhaCorreta) return res.status(401).send("Senha Incorreta")
+    //verificar se a senha digitada corresponde com a criptografada
+    const senhaCorreta = bcrypt.compareSync(senha, usuario.senha);
+    if (!senhaCorreta) return res.status(401).send("Senha Incorreta");
 
-  //se deu tudo certo, criar um token para enviar ao usuario
-  const token = uuid()
+    //se deu tudo certo, criar um token para enviar ao usuario
+    const token = uuid();
 
-  //guardar o token e o id do usuario para saber que ele esta logado
-  await db.collection("sessoes").insertOne(idUsuario: usuario._id, token)
+    //guardar o token e o id do usuario para saber que ele esta logado
+    await db.collection("sessoes").insertOne({ idUsuario: usuario._id, token });
 
-  //finalizar com status de sucesso e enviar token para o cliente
-  res.status(200).send(token)
-
-  } catch(err) {
-    res.status(500).send(err.message)
+    //finalizar com status de sucesso e enviar token para o cliente
+    res.status(200).send(token);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
-app.post("/nova-transacao/:tipo", async (req, res) => {
-  
-});
+app.post("/nova-transacao/:tipo", async (req, res) => {});
 
 app.get("/home", async (req, res) => {
   // o cliente envia um header de authorization com o token
-  const {authorization}= req.headers
+  const { authorization } = req.headers;
 
   // pegar o token sem a palavra Bearer
-  const token = authorization?.replace("Bearer", "")
+  const token = authorization?.replace("Bearer ", "");
 
   // se nao houver token, nao ha autozicao para continuar
-  if(!token) res.status(401).send("Token inexistente")
+  if (!token) res.status(401).send("Token inexistente");
 
-  try{
-
+  try {
     //caso o token exista, descobrir se é valido
-    const sessao = await db.collection("sessoes").findOne({token})
-    if (!sessao) return res.status(401).send("token inválido")
-    
-    // guardar o token e o id, tendo o id, procurar dados
-    const usuario = await db.collection("usuarios").findOne({_id:new ObjectId(sessao.idUsuario)})
-    
-    // o usuario possui _id, nome e senha, mas nao podemos enviar a senha
-    delete usuario.senha
-    
-    //enviar resposta
-    res.send(usuario)
+    const sessao = await db.collection("sessoes").findOne({ token });
+    if (!sessao) return res.status(401).send("Token inválido");
 
-} catch (err) {
-  res.status(500).send(err.message)
-}
+    // guardar o token e o id, tendo o id, procurar dados
+    const usuario = await db
+      .collection("usuarios")
+      .findOne({ _id: new ObjectId(sessao.idUsuario) });
+
+    // o usuario possui _id, nome e senha, mas nao podemos enviar a senha
+    delete usuario.senha;
+
+    //enviar resposta
+    res.send(usuario);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
   // db.collection("home")
-  //   .find()  
+  //   .find()
   //   .toArray()
   //   .then((home) => res.status(200).send(home))
   //   .catch((err) => res.status(500).send(err.message));
